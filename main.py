63098eb3c12baa1c399b3156bd8a030e199bc72b
Placius <krystekpol@tlen.pl>
2021-01-25 14:26:58 +0100

    start

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("würde gerne")
+# Createt by Krystian Płatek
+# 25.01.2021
+
+# git remote add origin https://github.com/Placius/fiszki.git
+# git branch -M main     
+# git push -u origin main
+
+# import modules
+
+# general class
+
+class Main:
+    def __init__(self):
+        pass
